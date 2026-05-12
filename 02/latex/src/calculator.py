90e23a8595756aadbd4d3c01a03ba852db3e6045
Michael V <mic.v-paul@gmx.de>
2026-05-12 14:45:10 +0200

    02 first draft done

--- 02/latex/src/calculator.py
+++ 02/latex/src/calculator.py
@@ -10,6 +10,15 @@ Uopa = np.sqrt(Utot**2 - Uscope**2)
 UopaErr = np.sqrt( (Utot*UtotErr/Uopa)**2 + (Uscope*UscopeErr/Uopa)**2 )
 print("Uopa", Uopa, UopaErr)
 
-u = Uopa/np.sqrt(deltaf)*1e3 # in nV/sqrt(Hz)
-uErr = UopaErr/np.sqrt(deltaf)*1e3
-print("u", u, uErr) 
+Eo = Uopa/np.sqrt(deltaf)*1e3 # in nV/sqrt(Hz)
+EoErr = UopaErr/np.sqrt(deltaf)*1e3
+print("u", Eo, EoErr) 
+
+
+fourkt = 1.6e-20*1e9
+R_f = 101e3*1e9 / 1e18 # TODO: remove /1e18
+G = 1001
+
+Eni = 1/G * np.sqrt(Eo**2 - fourkt*R_f*G)
+EniErr = Eo*EoErr/G**2/Eni
+print("Eni", Eni, EniErr)
