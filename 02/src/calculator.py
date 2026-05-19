edd51d0d9e9450376d2d25b3f3e4c1f5da8bdc3d
Michael V <mic.v-paul@gmx.de>
2026-05-19 16:17:13 +0200

    02 small improvements

--- 02/src/calculator.py
+++ 02/src/calculator.py
@@ -4,7 +4,7 @@ Utot = 25.15 # in uV
 UtotErr = 0.5
 Uscope = 3.35
 UscopeErr = 0.2
-deltaf = 5000
+deltaf = 5
 
 Uopa = np.sqrt(Utot**2 - Uscope**2)
 UopaErr = np.sqrt( (Utot*UtotErr/Uopa)**2 + (Uscope*UscopeErr/Uopa)**2 )
@@ -16,7 +16,7 @@ print("u", Eo, EoErr)
 
 
 fourkt = 1.6e-20*1e9
-R_f = 101e3*1e9 / 1e18 # TODO: remove /1e18
+R_f = 101e3*1e9 # TODO: remove /1e18
 G = 1001
 
 Eni = 1/G * np.sqrt(Eo**2 - fourkt*R_f*G)
